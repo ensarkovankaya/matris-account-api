ed116de08aad5644d9de6754c13fc9535f856d46
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-26 02:53:18 +0300

    add validateIf for optional fields

--- lib/grapgql/args/get.args.ts
+++ lib/grapgql/args/get.args.ts
@@ -1,21 +1,24 @@
-import { IsAlphanumeric, IsEmail, IsLowercase, IsString, Length } from 'class-validator';
+import { IsAlphanumeric, IsEmail, IsLowercase, IsString, Length, ValidateIf } from 'class-validator';
 import { IGetArgs } from '../models/args.model';
 import { Validatable } from '../validatable';
 
 export class GetArgs extends Validatable implements IGetArgs {
+    @ValidateIf(((object, value) => value !== undefined))
     @IsString()
     @Length(24, 24, {message: 'id must be equal to 24 characters'})
     public id?: string;
 
+    @ValidateIf(((object, value) => value !== undefined))
     @Length(4, 32, {message: 'InvalidLength'})
     @IsLowercase({message: 'NotLowercase'})
     @IsAlphanumeric({message: 'NotAlphanumeric'})
     public username?: string;
 
+    @ValidateIf(((object, value) => value !== undefined))
     @IsEmail()
     public email?: string;
 
-    constructor(data: { id?: string, username?: string, email?: string } = {}) {
+    constructor(data: { id?: string, username?: string, email?: string }) {
         super(data);
     }
 }
