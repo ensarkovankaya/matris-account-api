6191743e08ed1f486da350e1d5ae01d0230f40ed
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-26 00:28:41 +0300

    add validate if for optional fields

--- lib/grapgql/args/gender.query.ts
+++ lib/grapgql/args/gender.query.ts
@@ -1,4 +1,4 @@
-import { IsIn } from 'class-validator';
+import { IsIn, ValidateIf } from 'class-validator';
 import { Gender } from '../../models/gender.model';
 import { IGenderQueryModel } from '../../models/gender.query.model';
 import { Validatable } from '../validatable';
@@ -7,12 +7,14 @@ export class GenderQuery extends Validatable implements IGenderQueryModel {
     /**
      * Get users role equal to given role
      */
+    @ValidateIf(((object, value) => value !== undefined))
     @IsIn([Gender.MALE, Gender.FEMALE, Gender.UNKNOWN], {message: 'Invalid'})
     public eq?: Gender;
 
     /**
      * Get users one of given role.
      */
+    @ValidateIf(((object, value) => value !== undefined))
     @IsIn([Gender.MALE, Gender.FEMALE, Gender.UNKNOWN], {message: 'Invalid', each: true})
     public in?: Gender[];
 
--- lib/grapgql/args/role.query.ts
+++ lib/grapgql/args/role.query.ts
@@ -1,4 +1,4 @@
-import { IsIn } from 'class-validator';
+import { IsIn, ValidateIf } from 'class-validator';
 import { Role } from '../../models/role.model';
 import { Validatable } from '../validatable';
 
@@ -6,12 +6,14 @@ export class RoleQuery extends Validatable {
     /**
      * Get users role equal to given role
      */
+    @ValidateIf(((object, value) => value !== undefined))
     @IsIn([Role.ADMIN, Role.MANAGER, Role.INSTRUCTOR, Role.PARENT, Role.STUDENT])
     public eq?: Role;
 
     /**
      * Get users one of given role.
      */
+    @ValidateIf(((object, value) => value !== undefined))
     @IsIn([Role.ADMIN, Role.MANAGER, Role.INSTRUCTOR, Role.PARENT, Role.STUDENT], {each: true})
     public in?: Role[];
 
