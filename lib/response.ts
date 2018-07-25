5ed81db77b6c02519917ee8536facdda1e56b21b
Ensar Kovankaya <ensar@kovankaya.com>
2018-07-26 00:27:06 +0300

    change GraphQLError to IGraphQLError

--- lib/response.ts
+++ lib/response.ts
@@ -1,24 +1,24 @@
-import { GraphQLError } from 'graphql-request/dist/src/types';
 import { APIValidationError } from './error';
+import { IGraphQLError } from './grapgql/models/graphql.error.model';
 import { IAPIResponse } from './models/response.model';
 
 export class APIResponse<T> implements IAPIResponse<T> {
-    public errors: GraphQLError[];
+    public errors: IGraphQLError[];
     public status: number;
     public data: T;
 
-    constructor(status: number, data: T, errors: GraphQLError[] = []) {
+    constructor(status: number, data: T, errors: IGraphQLError[] = []) {
         this.status = status;
         this.errors = errors || [];
         this.data = data;
     }
 
     public hasErrors(): boolean {
-        return this.errors ? this.errors.length > 0 : false;
+        return this.errors.length > 0;
     }
 
     public hasError(name: string, raise: boolean = false): boolean {
-        const hasError = this.errors ? this.errors.filter(e => e.message === name).length > 0 : false;
+        const hasError = this.errors.filter(e => e.message === name).length > 0;
         if (hasError && raise) {
             throw new APIValidationError(this.errors);
         }
